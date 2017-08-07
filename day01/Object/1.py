f90bbebd87b6861212fe1885101d1e66915a41fd
hs <learning129@163.com>
2017-08-07 23:47:40 +0800

    格式

--- day01/Object/1.py
+++ day01/Object/1.py
@@ -1,10 +1,12 @@
 class Student(object):
 	"""docstring for Student"""
-	def __init__(self, name,sex):
-		self.name =name
-		self.sex =sex
-	def printSex(self):
-		print ('%s:%s'%(self.name,self.sex))
-		
-hu =Student('hushiwei','man')
-hu.printSex()
+	def __init__(self, name, sex):
+		self.name = name
+		self.sex = sex
+
+	def printsex(self):
+		print('%s:%s' % (self.name, self.sex))
+
+
+hu = Student('hushiwei', 'man')
+hu.printsex()
